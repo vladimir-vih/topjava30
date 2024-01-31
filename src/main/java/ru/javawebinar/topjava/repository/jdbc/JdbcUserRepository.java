65c2252181c6decaaee0f5127835d9ce7292d9d3
v.vikhlyantsev <vvikhlyantsev@humans.net>
2024-01-31 20:27:55 +0300

    HW06. added transactional annotation for the task 2.1

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -20,6 +21,7 @@ import java.util.List;
 import static java.util.Objects.requireNonNull;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepository extends JdbcRepositoryWithValidation implements UserRepository {
     private final JdbcTemplate jdbcTemplate;
 
@@ -38,6 +40,7 @@ public class JdbcUserRepository extends JdbcRepositoryWithValidation implements 
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         validateObject(user);
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
@@ -117,6 +120,7 @@ public class JdbcUserRepository extends JdbcRepositoryWithValidation implements 
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
