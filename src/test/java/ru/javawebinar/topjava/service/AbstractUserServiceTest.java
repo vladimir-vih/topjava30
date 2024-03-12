00dbc7e63d39eea8ff161e255399bccca50931e8
v.vikhlyantsev <vvikhlyantsev@humans.net>
2024-03-12 23:57:26 +0300

    HW08. fixed spaces

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -86,6 +86,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Set.of())));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Set.of())));
     }
+
     @Test
     void changeState() {
         assumeDataJpa();
