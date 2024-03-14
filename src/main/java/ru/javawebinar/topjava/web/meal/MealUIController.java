3886cb4ab19310908f714c37918508e16b0a0055
v.vikhlyantsev <vvikhlyantsev@humans.net>
2024-03-14 17:55:02 +0300

    HW08. refactored MealUIController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -1,19 +1,16 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/ajax/meals")
@@ -25,45 +22,26 @@ public class MealUIController extends AbstractMealController {
         return super.getAll();
     }
 
+    @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
-    @GetMapping("/update")
-    public String update(HttpServletRequest request, Model model) {
-        model.addAttribute("meal", super.get(getId(request)));
-        return "mealForm";
-    }
-
-    @GetMapping("/create")
-    public Meal create() {
-        return new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
-    }
-
     @PostMapping
-    public void updateOrCreate(@RequestParam String id,
-                               @RequestParam String dateTime,
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateOrCreate(@RequestParam @Nullable Integer id,
+                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
                                @RequestParam String description,
-                               @RequestParam String calories) {
-        Meal meal = new Meal(LocalDateTime.parse(dateTime), description, Integer.parseInt(calories));
-        if (id == null || id.isEmpty()) {
+                               @RequestParam int calories) {
+        Meal meal = new Meal(dateTime, description, calories);
+        if (id == null) {
             super.create(meal);
         } else {
-            super.update(meal, Integer.parseInt(id));
+            super.update(meal, id);
         }
     }
-
-   /* @GetMapping("/filter")
-    public String getBetween(HttpServletRequest request, Model model) {
-        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-        model.addAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
-        return "meals";
-    }*/
 
     @Override
     @GetMapping("/filter")
@@ -72,12 +50,6 @@ public class MealUIController extends AbstractMealController {
             @RequestParam @Nullable LocalTime startTime,
             @RequestParam @Nullable LocalDate endDate,
             @RequestParam @Nullable LocalTime endTime) {
-        List<MealTo> list = super.getBetween(startDate, startTime, endDate, endTime);
-        return list;
-    }
-
-    private int getId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
+        return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
