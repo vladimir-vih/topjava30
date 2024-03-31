b0fbacebbca1112d831ed4d365d0c72515dd9f40
vvikhlyantsev <vvikhlyantsev@humans.net>
2024-03-31 14:28:33 +0300

    HW09 removed unnecessary if (data.excess) in the topjava.meals.js

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -60,11 +60,7 @@ $(function () {
                 ]
             ],
             "createdRow": function (row, data, dataIndex) {
-                if (data.excess) {
-                    $(row).attr("data-meal-excess", true);
-                } else {
-                    $(row).attr("data-meal-excess", false);
-                }
+                $(row).attr("data-meal-excess", data.excess);
             }
         })
     );
